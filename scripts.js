85f8495b1256b6a2b73eca757c2196c370de0bac
turbocep <162988238+turbocep@users.noreply.github.com>
2024-05-17 13:58:40 +0200

    add 2 new playRound conditions.

--- scripts.js
+++ scripts.js
@@ -11,9 +11,25 @@ let humanScore = 0;
 let computerScore = 0;
 
 function playRound(humanChoice, computerChoice) {
-  if ((humanChoice == "rock" || humanChoice == "ROCK") && (computerChoice == "paper" || computerChoice == "PAPER")) {
+  //playerChoice > OR < OR == computerChoice
+  //rock < paper
+  if ((humanChoice == "rock" || humanChoice == "ROCK") && computerChoice == "paper") {
     console.log("You lose! Paper beats Rock.");
     computerScore++;
   }
+  // rock > scissors
+  else if ((humanChoice == "rock" || humanChoice == "ROCK") && computerChoice == "scissors") {
+    console.log("You win! Paper beats Scissors.");
+    humanScore++;
+  }
+  // rock == rock
+  else if ((humanChoice == "rock" || humanChoice == "ROCK") && computerChoice == "rock") {
+    console.log("Tie! You both picked rock.");
+  }
 }
+
+playRound("rock", "rock");
+
+console.log(humanScore);
+console.log(computerScore);
 
