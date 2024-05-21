bcde13ca22e767e2cb3477d13ea495d03a32a4c5
turbocep <162988238+turbocep@users.noreply.github.com>
2024-05-21 12:30:34 +0200

    add if statements for every choice combo.
    Rock-paper-scissors has a total of 9 possible
    combinations between the player and computer. An
    if statement was made for each one. I know there
    must be a more efficient way of doing this but I
    haven't planned it yet. I'll probably create a
    feature branch for it to test it.

--- scripts.js
+++ scripts.js
@@ -10,6 +10,8 @@ function getHumanChoice() {
 let humanScore = 0;
 let computerScore = 0;
 
+
+//Rewrite into nested IF statements. 
 function playRound(humanChoice, computerChoice) {
   //playerChoice > OR < OR == computerChoice
   //rock < paper
@@ -24,12 +26,41 @@ function playRound(humanChoice, computerChoice) {
   }
   // rock == rock
   else if ((humanChoice == "rock" || humanChoice == "ROCK") && computerChoice == "rock") {
-    console.log("Tie! You both picked rock.");
+    console.log("Tie! You both picked Rock.");
   }
+  // paper > rock
+  else if ((humanChoice == "paper" || humanChoice == "PAPER") && computerChoice == "rock") {
+    console.log("You win! Paper beats Rock.");
+    humanScore++;
+  }
+  // paper == paper
+  else if ((humanChoice == "paper" || humanChoice == "PAPER") && computerChoice == "paper") {
+    console.log("Tie! You both picked Paper.");
+  }
+  // paper < scissors
+  else if ((humanChoice == "paper" || humanChoice == "PAPER") && computerChoice == "scissors") {
+    console.log("You lose! Scissors beat Paper.");
+    computerScore++;
+  } 
+  // scissors < rock
+  else if ((humanChoice == "scissors" || humanChoice == "SCISSORS") && computerChoice == "rock") {
+    console.log("You lose! Rock beats Scissors.");
+    computerScore++;
+  }
+  // scissors > paper
+  else if ((humanChoice == "scissors" || humanChoice == "SCISSORS") && computerChoice == "paper") {
+    console.log("You win! Scissors beat paper.");
+    playerScore++;
+}
+// scissors == scissors
+else if ((humanChoice == "scissors" || humanChoice == "SCISSORS") && computerChoice == "scissors") {
+  console.log("Tie! You both picked scissors.");
+}
 }
 
 playRound("rock", "rock");
 
 console.log(humanScore);
 console.log(computerScore);
+
 
