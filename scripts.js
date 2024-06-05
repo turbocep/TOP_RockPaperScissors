9a7285a5f678a46fd2450063301bafbcd52f4623
turbocep <162988238+turbocep@users.noreply.github.com>
2024-06-05 12:47:17 +0200

    add event listeners for choice buttons.

--- scripts.js
+++ scripts.js
@@ -9,23 +9,6 @@ function updateScore() {
   outScore.textContent = `${playerScore}:${botScore}`;
 }
 
-//Getting DOM references
-const buttons = document.querySelectorAll("button");
-
-const rockRef = buttons[0];
-const paperRef = buttons[1];
-const scissorsRef = buttons[2];
-
-const outScore = document.querySelector(".score");
-const outPlays = document.querySelector(".plays");
-const outResult = document.querySelector(".result");
-
-let playerScore = 0;
-let botScore = 0;
-
-let playerChoice;
-
-
 //You input what the player and bot picked and it returns a string
 //declaring who won. "bot", "player" and "tie" are the options.
 function getWinner(playerChoice, botChoice) {
@@ -52,6 +35,9 @@ function getWinner(playerChoice, botChoice) {
   }
 }
 
+//Plays one round. This includes getting bot's choice, comparing it
+//to player's and deciding the result. The score is incremented and
+//the result is printed out. 
 function playRound(playerChoice) {
   const botChoice = getComputerChoice();
   const resultOfRound = getWinner(playerChoice, botChoice);
@@ -65,12 +51,42 @@ function playRound(playerChoice) {
   } else {
     outResult.textContent = "It's a tie!";
   }
+  updateScore();
+  if (playerScore == 5 || botScore == 5) {
+    //end game function. Turn-off button events and display final winner.
+  }
 }
 
+//Adding event listeners for buttons. 
+rockRef.addEventListener("click", () => {
+  playRound("rock");
+})
+paperRef.addEventListener("click", () => {
+  playRound("paper");
+})
+scissorsRef.addEventListener("click", () => {
+  playRound("scissors");
+})
+
+
+//Getting DOM references
+const buttons = document.querySelectorAll("button");
+
+const rockRef = buttons[0];
+const paperRef = buttons[1];
+const scissorsRef = buttons[2];
+
+const outScore = document.querySelector(".score");
+const outPlays = document.querySelector(".plays");
+const outResult = document.querySelector(".result");
+
+let playerScore = 0;
+let botScore = 0;
 
 
 
-//Function: Who played what message.
+
+
 
 
 
