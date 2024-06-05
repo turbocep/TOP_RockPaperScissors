905330620df070ef6719bc7b87d0a4f96c3d362c
turbocep <162988238+turbocep@users.noreply.github.com>
2024-06-05 12:26:10 +0200

    complete getWinner function.

--- scripts.js
+++ scripts.js
@@ -23,21 +23,41 @@ const outResult = document.querySelector(".result");
 let playerScore = 0;
 let botScore = 0;
 
-rockRef.addEventListener("click", () => {
-  return "rock";
-})
+let playerChoice;
 
-paperRef.addEventListener("click", () => {
-  return "paper";
-})
 
-scissorsRef.addEventListener("click", () => {
-  return "scissors";
-})
+//You input what the player and bot picked and it returns a string
+//declaring who won. "bot", "player" and "tie" are the options.
+function getWinner(playerChoice, botChoice) {
+  if (playerChoice == botChoice) {
+    return "tie";
+  } else if (playerChoice == "rock") {
+    if (botChoice == "paper") {
+      return "bot";
+    } else if (botChoice == "scissors") {
+      return "player";
+    }
+  } else if (playerChoice == "paper") {
+    if (botChoice == "rock") {
+      return "player";
+    } else if (botChoice == "scissors") {
+      return "bot";
+    }
+  } else if (playerChoice == "scissors") {
+    if (botChoice == "rock") {
+      return "bot";
+    } else if (botChoice == paper) {
+      return "player";
+    }
+  }
+}
+
 
 
 
 //Function: Who played what message.
+
+//rock > scissors > paper > rock;
 
 //Function: Who won the round. 
 
